2ef2dd287064c5280a7b4c33f64a49225c607d5a
akash <akashpatil42635@gmail.com>
2025-02-15 16:15:46 +0530

    2 files commited

--- User.java
+++ User.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
